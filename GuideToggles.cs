f494df478dfc5d93e466c43020fc6225245d637a
xenofite <>
2024-09-07 13:02:00 -0500

    russian localization from discord user @xenofite

--- GuideToggles.cs
+++ GuideToggles.cs
@@ -1,7 +1,6 @@
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using System.Collections.Generic;
-using Terraria;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
